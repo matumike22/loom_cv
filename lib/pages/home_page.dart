b6278c97d9fc353f53d44a4c49d2ecbdc9bcb230
matumike22 <matumike22@gmail.com>
2025-12-15 21:26:48 +0000

    firebase logic AI added

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_ai/firebase_ai.dart';
 import 'package:flutter/material.dart';
 
 class HomePage extends StatelessWidget {
@@ -7,7 +8,27 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(title: const Text('Home Page')),
-      body: const Center(child: Text('Welcome to the Home Page!')),
+      body: Center(
+        child: ElevatedButton(
+          onPressed: () async {
+            // Initialize the Gemini Developer API backend service
+            // Create a `GenerativeModel` instance with a model that supports your use case
+            final model = FirebaseAI.googleAI().generativeModel(
+              model: 'gemini-2.5-flash',
+            );
+
+            // Provide a prompt that contains text
+            final prompt = [
+              Content.text('Write a story about a magic backpack.'),
+            ];
+
+            // To generate text output, call generateContent with the text input
+            final response = await model.generateContent(prompt);
+            print(response.text);
+          },
+          child: Text('Press Me'),
+        ),
+      ),
     );
   }
 }
